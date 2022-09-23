88b7bc2319ce71f0b22b23e1c2c7aebac5e0f2a4
Iskandar <seik747@gmail.com>
2022-09-23 22:22:35 +0000

    defining the SQLAlche diagram

--- src/models.py
+++ src/models.py
@@ -8,6 +8,55 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    user_name = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+    favorites = relationship("Favorites")
+
+    def log_in(self):
+        return None
+
+    def favorites(self):
+        return None
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String(50), nullable=False)
+    birth_year = Column(String(50), nullable=False)
+    gender = Column(String(50), nullable=False)
+    skin_color = Column(String(50), nullable=False)
+    heigth = Column(String(50), nullable=False)
+    favorites = relationship("Favorites")
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(50), nullable=False)
+    diameter = Column(String(50), nullable=False)
+    climate = Column(String(50), nullable=False)
+    terrain = Column(String(50), nullable=False)
+    population = Column(String(50), nullable=False)
+    favorites = relationship("Favorites")
+
+class Favorites(Base):
+    __tablename__ = 'add_favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+
+
+
+
+
+
+################ Old stuff ##################
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
