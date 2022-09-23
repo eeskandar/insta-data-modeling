8c54c27637d72c19f52ecefffc6fddcade67722d
Iskandar <seik747@gmail.com>
2022-09-23 23:34:45 +0000

    defining the diagram from SQLAlchemy

--- src/models.py
+++ src/models.py
@@ -13,70 +13,85 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
+    phone_number = Column(String(50))
     email = Column(String(50), nullable=False)
     user_name = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    follower = relationship("Follower")
     favorites = relationship("Favorites")
 
-    def log_in(self):
+    def follower(self):
+        return None
+    
+    def update(self):
         return None
 
-    def favorites(self):
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    media = relationship("Media")
+    favorites = relationship("Favorites")
+
+    def update(self):
         return None
 
-class Character(Base):
-    __tablename__ = 'character'
+
+class Comment(Base):
+    __tablename__ = 'comment'
     id = Column(Integer, primary_key=True)
-    character_name = Column(String(50), nullable=False)
-    birth_year = Column(String(50), nullable=False)
-    gender = Column(String(50), nullable=False)
-    skin_color = Column(String(50), nullable=False)
-    heigth = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    content = Column(String(250), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     favorites = relationship("Favorites")
 
-class Planet(Base):
-    __tablename__ = 'planet'
+    def update(self):
+        return None
+
+
+class Media(Base):
+    __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    planet_name = Column(String(50), nullable=False)
-    diameter = Column(String(50), nullable=False)
-    climate = Column(String(50), nullable=False)
-    terrain = Column(String(50), nullable=False)
-    population = Column(String(50), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    url = Column(String(250), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     favorites = relationship("Favorites")
+
+    def update(self):
+        return None
+
 
 class Favorites(Base):
     __tablename__ = 'add_favorites'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
-    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
+    def update(self):
+        return None
+    
 
-
-
-
-
-################ Old stuff ##################
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    id_from_user = Column(Integer, ForeignKey('user.id'), nullable=False)
+    id_to_user = Column(Integer, ForeignKey('user.id'), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+    def update(self):
+        return None
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
